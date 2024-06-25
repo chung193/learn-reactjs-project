27b0eb173ee63da5a2577a2d1af57dff4ef7b3ed
chung193 <chungvd.it@gmail.com>
2024-06-25 19:52:49 +0700

    a little change

--- src/screens/Weather.js
+++ src/screens/Weather.js
@@ -7,7 +7,7 @@ const Weather = ()=>{
 
     const fetchData = async (lat, lon) => {
         try {
-          const response = await fetch('http://api.weatherapi.com/v1/current.json?key=74ef00a2e39b422fad3160613242406&q='+lat+','+lon);
+          const response = await fetch('http://api.weatherapi.com/v1/current.json?aqi=yes&key=74ef00a2e39b422fad3160613242406&q='+lat+','+lon);
           if (!response.ok) {
             setNoti('Network response was not ok');
             throw new Error('Network response was not ok');
@@ -40,30 +40,44 @@ const Weather = ()=>{
       <div className='weather'>
       <p>{noti}</p>
       <p>Thời tiết hiện tại</p>
-      <Grid container spacing={2} sx={{ flexGrow: 1 }}>
-      <Grid xs={6}>
+      {
+        weather && 
+        (
+        <><Grid container spacing={2} sx={{ flexGrow: 1 }}>
+        <Grid xs={6}>
+         <img src={weather.current?.condition?.icon}/>
+        <p>{weather.current?.condition?.text}</p>
+        </Grid>
+        <Grid xs={6}>
+        <p>{weather.location?.name}, {weather.location?.country}</p>
+        <p>Update: {weather.location?.localtime}</p>
+        </Grid>
+        </Grid>
+        <Grid container spacing={2} sx={{ flexGrow: 1 }}>
+        <Grid xs={3}>
+        <p>Nhiệt độ: {weather.current?.dewpoint_c}°C</p>
+        <p>Cảm giác như: {weather.current?.feelslike_c}°C</p>
+        </Grid>
+        <Grid xs={3}>
+        <p>Sức gió: {weather.current?.gust_kph} km/h</p>
+        </Grid>
+        <Grid xs={3}>
+        <p>Tổng lượng mưa (mm): {weather.current?.precip_mm} mm</p>
+        </Grid> 
+        <Grid xs={3}>
+        <p><strong>Chất lượng không khí:</strong> <br/>
+        Carbon Monoxide (μg/m3) <strong>{weather.current?.air_quality?.co}</strong> <br/>
+        Ozone (μg/m3) <strong>{weather.current?.air_quality?.o3}</strong> <br/>
+        Nitrogen dioxide (μg/m3) <strong>{weather.current?.air_quality?.no2}</strong> <br/>
+        Sulphur dioxide (μg/m3) <strong>{weather.current?.air_quality?.so2}</strong> <br/>
+        PM2.5 (μg/m3) <strong>{weather.current?.air_quality?.pm2_5}</strong> <br/>
+        PM10 (μg/m3) <strong>{weather.current?.air_quality?.pm10}</strong> <br/>
+        </p>
+        </Grid> 
+      </Grid></>
+      )
+      }
       
-      <img src={weather.current?.condition?.icon}/>
-      <p>{weather.current?.condition?.text}</p>
-      </Grid>
-      <Grid xs={6}>
-      <p>{weather.location?.name}, {weather.location?.country}</p>
-      <p>update: {weather.location?.localtime}</p>
-      </Grid>
-      </Grid>
-      
-      <Grid container spacing={2} sx={{ flexGrow: 1 }}>
-      <Grid xs={4}>
-      <p>Nhiệt độ: {weather.current?.dewpoint_c}°C</p>
-      <p>Cảm giác như: {weather.current?.feelslike_c}°C</p>
-      </Grid>
-      <Grid xs={4}>
-      <p>Sức gió: {weather.current?.gust_kph} km/h</p>
-      </Grid>
-      <Grid xs={4}>
-      <p>Tổng lượng mưa (mm): {weather.current?.precip_mm} mm</p>
-      </Grid>
-    </Grid>
     </div>
     )
 }
